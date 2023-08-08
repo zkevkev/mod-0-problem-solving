6cea3dfb059a75182f45f65bc65c926c01781241
Kevin Zolman <kevin.zolman@gmail.com>
2023-08-08 09:31:09 -0600

    Update pseudocode description in challenge1.rb

--- challenge1.rb
+++ challenge1.rb
@@ -2,7 +2,7 @@
 # Define array
 # Use each method to act on individual elements
 # Use length method to get string lengths
-# Use conditional statement to print if length is equal to 4 (nest length method within this logic)
+# Use conditional statement to print interpolated string if length is equal to 4 (nest length method within this logic)
 
 names = ["John", "Jane", "Jim", "Jacky"]
 
@@ -10,4 +10,4 @@ names.each do |name|
     if name.length == 4
         puts "#{name}"
     end
-endß
+end
