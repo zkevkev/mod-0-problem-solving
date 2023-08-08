5564a810d6c104e9fa3abbafd13e867a21af1eb9
Kevin Zolman <kevin.zolman@gmail.com>
2023-08-08 09:28:53 -0600

    Add completed challenge1.rb

---
+++ challenge1.rb
@@ -0,0 +1,13 @@
+# Create an array containing string elements. Print the strings that are 4 characters long
+# Define array
+# Use each method to act on individual elements
+# Use length method to get string lengths
+# Use conditional statement to print if length is equal to 4 (nest length method within this logic)
+
+names = ["John", "Jane", "Jim", "Jacky"]
+
+names.each do |name|
+    if name.length == 4
+        puts "#{name}"
+    end
+endß
